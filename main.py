c34d6e0a5ecfa8c2092b00f7302f221d164c983c
Sergio Baltazar <baltazarsergiodavid@gmail.com>
2023-01-12 19:07:52 -0400

    Initial commit python

---
+++ main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    #print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+#if __name__ == '__main__':
+   # print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# First basic module phyton, define variables
+# "str" change the variable "int" to "String"
+
+nombre = 'Sergio'
+edad = 33
+print( 'Hola mundo, soy ' + nombre + ' y tengo ' +str(edad) + ' años')
+
+#we can save the information in the variable like this
+
+nombre = 'David'
+text = 'Hola mundo, soy ' + nombre + ' y tengo ' +str(edad) + ' años'
+print(text)
---
+++ operators.py
@@ -0,0 +1,13 @@
+# verify is edad is more than 18
+edad = 18
+if edad >= 18:
+    print('es mayor de edad')
+else:
+    print('es menor de edad')
+
+# verify is edad is less than 15
+edad = 15
+if edad <= 18:
+    print('es menor de edad')
+else:
+    print('es mayor de edad')
