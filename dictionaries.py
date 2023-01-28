ed2aa4c24cbc56e92f5c4f85b3a109901bfccfa7
Sergio Baltazar <baltazarsergiodavid@gmail.com>
2023-01-27 20:07:12 -0400

    Applying dictionaries new functions part 1

--- dictionaries.py
+++ dictionaries.py
@@ -41,10 +41,60 @@ print(ooo)
 
 # What does dict equal after running this code?:
 print('----------------------------------------')
-dict = {"Fri": 20, "Thu": 6, "Sat": 1}
-dict["Thu"] = 13
-dict["Sat"] = 2
-dict["Sun"] = 9
-print(dict)
+#dict = {"Fri": 20, "Thu": 6, "Sat": 1}
+#dict["Thu"] = 13
+#dict["Sat"] = 2
+#dict["Sun"] = 9
+#print(dict)
 
 # MOST COMMON NAME?
+# marquard, zhen, csev, sven
+
+# Many Counters with a Dictionary
+# One common use of dictionaries is counting how often we "see" something
+print('----------------------------------------')
+ccc = dict()
+ccc['csev'] = 1
+ccc['cwen'] = 1
+print(ccc)
+ccc['cwen'] = ccc['cwen'] + 1
+print(ccc)
+
+# Dictionary Traceback
+# We can use the 'in' operator to see if a key is in the dictionary
+print('----------------------------------------')
+ccc = dict()
+print('csev' in ccc)
+
+# When we see a new name
+print('----------------------------------------')
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+    if name not in counts:
+        counts[name] = 1
+    else:
+        counts[name] = counts[name] + 1
+print(counts)
+
+# The get method for dictionaries
+print('----------------------------------------')
+if name in counts:
+    x = counts[name]
+else:
+    x = 0
+x = counts.get(name, 0)
+print(x)
+
+# Simplified counting with get()
+print('----------------------------------------')
+counts = dict()
+names = ['csev', 'cwen', 'csev', 'zqian', 'cwen']
+for name in names:
+   counts[name] = counts.get(name, 0) + 1
+print(counts)
+
+# What will the following code print?
+print('----------------------------------------')
+counts = { 'quincy' : 1 , 'mrugesh' : 42, 'beau': 100, '0': 10}
+print(counts.get('kris', 0))
